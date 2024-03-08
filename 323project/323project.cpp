5afc84c7117125d1728215b8702288b70b173ab1
knguyen196 <kentnguyen196@gmail.com>
2024-03-07 19:02:03 -0800

    Revert "KN - added regex to remove whitespaces"
    This reverts commit 76535b389ad488e65b519e59dd79dc221493a6f8.

--- 323project/323project.cpp
+++ 323project/323project.cpp
@@ -1,24 +1,9 @@
 #include <iostream>
 #include <fstream>
-#include <vector>
-#include <string>
-#include <regex>
 using namespace std;
 
 int main() {
 	string text;
-
-	// vectors used to sort tokens into their categories
-	vector<string> keywords, identifiers, literals,
-		operators, delimiters, whitespace;
-
-
-	/*
-	 You can use "323example.txt" or "323example2.txt"
-
-	 "323example2.txt" is the same as the one provided in
-	 the project file, "323example.txt" is more complicated
-	*/
 	ifstream ReadFile("323example.txt");
 
 	if (!ReadFile) {
@@ -26,34 +11,10 @@ int main() {
 		return 1;
 	}
 
-	// receive text from file
 	while (getline(ReadFile, text)) {
-		// Skip comment lines
-		if (text[0] == '#' || (text[0] == '/' && text[1] == '/')) continue;
-
-		// Using regex to remove whitespaces
-		text = std::regex_replace(text, std::regex("\\s+"), "");
 		cout << text << endl;
+		}
 
-		// Find certain strings and sort them into the
-		// corresponding vectors
-
-		// Regular expressions ? (regex)
-
-
+		ReadFile.close();
+		return 0;
 	}
-
-	// Close File
-	ReadFile.close();
-
-
-
-	// Output - unfinished
-	cout << "\nOutput 1 - Code after removing excess space & comments:" << endl;
-
-
-	cout << "Output 2 - Tokenized code:" << endl;
-
-	return 0;
-
-}
